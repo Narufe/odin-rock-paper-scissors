ad1db3f2d8097a94770c5750c7f19f617f4a3335
Rafe Taya <rafe.otaya@gmail.com>
2024-05-05 08:46:17 +0300

    Create function getComputerChoice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+function getComputerChoice() {
+  let ranNum = Math.floor((Math.random() * 100)) + 1
+  console.log(ranNum);
+  if (ranNum >= 1 && ranNum <= 33) {
+    return "Rock";
+  } else if (ranNum >= 34 && ranNum <= 66) {
+    return "Paper";
+  } else if (ranNum >= 67 && ranNum < 100) {
+    return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
+
