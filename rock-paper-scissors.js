c85c8b1b0f22fa08408233b1500159c92fcff690
Rafe Taya <rafe.otaya@gmail.com>
2024-05-05 12:09:31 +0300

    Create function playRound based on the game rules

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,21 +5,55 @@ function getComputerChoice() {
   let ranNum = Math.floor((Math.random() * 100)) + 1
   // console.log(ranNum);
   if (ranNum >= 1 && ranNum <= 33) {
-    return "Rock";
+    return "rock";
   } else if (ranNum >= 34 && ranNum <= 66) {
-    return "Paper";
+    return "paper";
   } else if (ranNum >= 67 && ranNum < 100) {
-    return "Scissors";
+    return "scissors";
   }
 }
-
-// console.log(getComputerChoice());
 
 function getHumanChoice() {
   return(prompt("Please key in your choice", ""));
 }
 
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+    
+  if (computerChoice === "rock" && humanChoice === "scissors") {
+    computerScore++;
+    console.log("You lose! Rock beats scissors");
+  } else if (computerChoice === "rock" && humanChoice === "paper") {
+    humanScore++;
+    console.log("You win! Paper beats rock");
+  } else if (computerChoice === "paper" && humanChoice === "scissors") {
+    humanScore++;
+    console.log("You win! scissors beat paper");
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    computerScore++;
+    console.log("You lose! Paper beat rock");
+  } else if (computerChoice === "scissors" && humanChoice === "rock") {
+    humanScore++;
+    console.log("You win. rock beat scissors");
+  } else if (computerChoice === "scissors" && humanChoice === "paper") {
+    computerScore++;
+    console.log("You lose! scissors beat paper");
+  } else {
+      console.log("repeat");
+  }
+
+}
+
+
+const humanSelection = getHumanChoice();
+console.log(humanSelection);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+
+playRound(humanSelection, computerSelection);
+
 
 
 
